355ebb7d9d9476089a3df469ce4bb3d5a1f7f71a
Adarsh Verma <55326493+Tryingtroyl@users.noreply.github.com>
2022-12-20 10:07:55 +0530

    Update access token

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 node {
   try {
     stage('Deploy') {
-      sh("curl -u sqa_46ee6fd0572c2b9dcf392015aac477ebaf9be130: -d 'projectKey=$projectName&gateName=Lowest-QG' -X POST http://3.108.228.16:9000/api/qualitygates/select")
+      sh("curl -u squ_f1e4ff083b9e22a1bcf7b83d3fb8f471784136bd: -d 'projectKey=$projectName&gateName=Lowest-QG' -X POST http://3.108.228.16:9000/api/qualitygates/select")
       def scannerHome = tool 'SonarQube'
       def branch = 'dev'
       def path = 'files'
