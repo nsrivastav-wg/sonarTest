95b94e08dc7acc02c39e436646984ef46b8858d4
Adarsh Verma <55326493+Tryingtroyl@users.noreply.github.com>
2022-12-20 10:08:41 +0530

    Update path of files

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node {
       sh("curl -u squ_f1e4ff083b9e22a1bcf7b83d3fb8f471784136bd: -d 'projectKey=$projectName&gateName=Lowest-QG' -X POST http://3.108.228.16:9000/api/qualitygates/select")
       def scannerHome = tool 'SonarQube'
       def branch = 'dev'
-      def path = 'files'
+      def path = './'
       def verbose = false
       withSonarQubeEnv('SonarQube'){
     sh("${scannerHome}/bin/sonar-scanner -Dsonar.branch.name=${branch} -Dsonar.projectKey=${projectName} -Dsonar.sources=${path} -Dsonar.java.binaries=${path} -Dsonar.verbose=${verbose}")
